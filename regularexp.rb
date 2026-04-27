d8661e2d1ccce71827a85d9b859de541f92b71f1
rahul <rahulsinghxtech@gmail.com>
2026-04-27 18:57:39 +0530

    regex done

--- regularexp.rb
+++ regularexp.rb
@@ -12,14 +12,122 @@ puts  "hello" =~ /llo/
 
 
  #common Regex pattern 
-
  #(.) any one character 
  puts "xxcat" =~ /.t/ 
 
 
- #digit (\d)
+ #digit(1234567) (\d)
  puts "abcde4rrr" =~ /\d/ 
 
 
 #  word  character (\w)
 puts "@@@hellllllo" =~ /\w/ 
+
+
+#( \s ) space 
+puts "hello world!!!!!" =~ /\s/
+
+
+# (^) start of string 
+puts "hiiii Hello" =~ /^hiiii/ 
+
+
+# ($) ending of string 
+puts "hiii hello" =~ /hello$/
+
+
+#QUantifiers (repetation)
+# (*) character can appear zero OR many times
+puts "dooooooog" =~ /d*og/
+
+
+
+# (+) 
+puts "ct" =~ /ca+t/ 
+puts "cat" =~ /ca+t/
+puts "caaaat" =~ /ca+t/
+
+
+
+# (?) 
+puts  "dg" =~ /do?g/
+puts "dog" =~ /do?g/
+puts "doooog" =~ /do?g/
+
+
+
+# {n} must aappear how many times 
+puts "dooog" =~ /do{3}g/
+puts "doog" =~ /do{3}g/
+
+
+
+# email validation 
+email = "rahul@gmail.com"
+if email =~ /\w+@\w+\.\w+/
+puts " valid email"
+else 
+puts "invalid email"
+end 
+
+
+
+#phone number 
+phone = "8090220080"
+if phone =~ /^\d{10}$/
+	puts "valid number"
+else 
+	puts "invalid number"
+end 
+
+
+
+#substitution (gsub)
+message = "i like javascript"
+puts message.gsub("javascript", "Ruby")
+
+
+
+# scam method 
+text = "rat mat bat"
+puts text.scan(/.at/)
+
+
+
+#split using regex
+text = "house;apartment,home hut"
+puts text.split(/[,; ]/)
+
+
+
+# Anchors (^ start and $ end) 
+puts "Hello" =~ /^H/
+puts "hiiiii" =~ /i$/
+
+
+
+#Groups ()
+text = "my number is 809022"
+match = text.match(/(\d+)/)
+puts match[0]
+
+
+# Extract numbers
+text = "Order Id: 5678 , Price: 250 "
+numbers = text.scan(/\d+/)
+puts numbers
+
+
+#case sensitive  (ignore upper case lower case )
+puts "Hello" =~ /hello/i
+
+
+
+#Password Validation 
+
+password = "abc123456"
+if password =~ /^(?=.*\d).{6,}$/
+	puts "strong password"
+else 
+	puts "weak password"
+end 
